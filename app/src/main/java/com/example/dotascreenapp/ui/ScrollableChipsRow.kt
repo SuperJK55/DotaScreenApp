497fe5329922f646e9c6c56142898c165e0774ee
SuperJK55 <stepan.kolenkin@mail.ru>
2023-11-03 00:44:25 +0600

    Добавил тэги для приложения

--- app/src/main/java/com/example/dotascreenapp/ui/ScrollableChipsRow.kt
+++ app/src/main/java/com/example/dotascreenapp/ui/ScrollableChipsRow.kt
@@ -6,11 +6,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dotascreenapp.ui.theme.DotaAppTheme
 
@@ -47,4 +47,12 @@ private fun Chip(text: String) {
             modifier = Modifier.padding(top = 5.dp, bottom = 5.dp, start = 10.dp, end = 10.dp)
         )
     }
+}
+@Preview
+@Composable
+fun ScrollableChipsRowPreview(){
+    ScrollableChipsRow(
+        items = listOf("MOBA", "MULTYPLAYER", "STRATEGY"),
+        modifier = Modifier,
+        contentPadding = PaddingValues(start = 24.dp, end = 24.dp, top = 20.dp, bottom = 16.dp))
 }
